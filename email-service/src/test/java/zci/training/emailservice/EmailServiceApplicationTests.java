6bfec1134047be70322ec28b91025fbee634a959
Zakaria CHAFI <zchafi@gmail.com>
2025-03-07 10:05:13 +0000

    Event-Driven Microservices using Spring Boot and Kafka

---
+++ email-service/src/test/java/zci/training/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zci.training.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- order-service/src/main/java/zci/training/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/zci/training/orderservice/kafka/OrderProducer.java
@@ -1,11 +1,11 @@
-package zci.training.stockservice.kafka;
+package zci.training.orderservice.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.common.protocol.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import zci.training.basedomains.dto.OrderEvent;
